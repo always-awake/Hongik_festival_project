e1157dc4dcd45ac470c47623f5c0bed6ab25035a
leemirim <leemirim9654@gmail.com>
2019-05-08 19:15:15 +0900

    User 모델 생성

--- festival/users/models.py
+++ festival/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
- 
+
+class User(models.Model):
+    """ User Model """
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified'),
+    )
+    profile_image = models.ImageField(blank=True, null=True)
+    name = models.CharField(max_length=255, null=True)
+    bio = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=140, blank=True, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+
+    def __str__(self):
+        return self.username
+    
