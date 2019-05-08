18b69d0cae9b29d97ff9708a8d49992be11294ca
leemirim <leemirim9654@gmail.com>
2019-05-08 22:37:33 +0900

    Bar 모델 생성 및 어드민 패널에 추가

--- festival/bars/admin.py
+++ festival/bars/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Bar)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'text',
+    )
--- festival/bars/models.py
+++ festival/bars/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from festival.posts.models import TimeStampedModel
 
-# Create your models here.
+
+class Bar(TimeStampedModel):
+    """ Bar Model """
+    image = models.ImageField(upload_to=f'bars/')
+    name = models.CharField(max_length=80)
+    text = models.TextField(max_length=200) # 술집에 대한 간단한 소개
+    location_url = models.TextField(max_length=200) # 술집 위치 -> 이후 필요에 따라 변경될 가능성이 높은 필드
+
+    def __str__(self):
+        return f'{self.name}'
+
+# 입시 모델 - 필요에 따라 삭제될 수 있는 모델
+class BarLike(TimeStampedModel):
+    """ BarLike Model """
+    bar = models.ForeignKey(Bar, on_delete=models.CASCADE, related_name='likes')
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL)
