c140e9f571c860882e741c6e7258f4877d44f938
ffanggyu <rbghks0327@gmail.com>
2019-05-10 16:41:15 +0900

    asdf

--- festival/bars/models.py
+++ festival/bars/models.py
@@ -18,3 +18,7 @@ class BarLike(TimeStampedModel):
     """ BarLike Model """
     bar = models.ForeignKey(Bar, on_delete=models.CASCADE, related_name='likes')
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+
+
+    dfg
