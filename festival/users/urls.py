3fb54537976683b163b06c796d70f99f294b0c74
leemirim <leemirim9654@gmail.com>
2019-05-11 17:58:40 +0900

    User 모델 수정에 따른 signup뷰 수정

--- festival/users/urls.py
+++ festival/users/urls.py
@@ -7,10 +7,13 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.mypage, name='mypage'),
+    path('', views.user_list, name='user_list'),
+    path('mypage/', views.mypage, name='mypage'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('letters/<int:user_id>/', views.letter_post, name='letter_post'),
+    path('letters/to/', views.letter_to_list, name='letter_to_list'),
+    path('letters/from/', views.letter_from_list, name='letter_from_list'), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- festival/users/views.py
+++ festival/users/views.py
@@ -11,10 +11,15 @@ def mypage(request):
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(username=request.POST['username'],
-            name=request.POST.get('name'), password=request.POST['password1'], bio=request.POST['bio'],
-            phone=request.POST['phone'], gender=request.POST['gender'], profile_image=request.FILES['profile_img']
-            )
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                # name=request.POST.get('name'), 
+                password=request.POST['password1'], 
+                profile_image=request.FILES['profile_img'],
+                bio=request.POST['bio'],
+                #phone=request.POST['phone'], 
+                gender=request.POST['gender'], 
+                )
             if user is not None:
                 auth.login(request, user)
                 return redirect('http://localhost:8000/users')
@@ -33,14 +38,13 @@ def login(request):
     else:
         return render(request, 'login.html')
 
-
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         redirect('home')
     return render(request, 'home.html')
 
-
+# 쪽지를 보내는 함수
 def letter_post(request, user_id):
     if request.user.is_authenticated and request.method == 'POST':
         form = LetterPost(request.POST)
@@ -53,3 +57,18 @@ def letter_post(request, user_id):
     elif request.method == 'GET':
         form = LetterPost()
         return render(request, 'letter_post.html', {'form': form})
+
+# 유저가 받은 쪽지함
+def letter_to_list(request):
+    letter_to_list = Letter.objects.filter(letter_to=request.user)
+    return render(request, 'letter_to_detail.html', {'letters':letter_to_list})
+
+# 유저가 보낸 쪽지함
+def letter_from_list(request):
+    letter_from_list = Letter.objects.filter(letter_from=request.user)
+    return render(request, 'letter_from_detail.html', {'letters':letter_from_list})
+
+def user_list(request):
+    users = User.objects.all()
+    # 페이지네이션 코드 구현
+    return render(request, 'user_list.html', {'users':users})
