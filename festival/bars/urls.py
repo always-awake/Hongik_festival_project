011f54ab36e7e76374b5197598b6b82297a1c128
leemirim <leemirim9654@gmail.com>
2019-05-11 12:16:05 +0900

    bar_list 페이지를 위한 뷰, 템플릿, url 설정

--- festival/bars/urls.py
+++ festival/bars/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'bars'
 urlpatterns = [
-
+    path('', views.bar_list, name='bar_list')
 ]
--- festival/bars/views.py
+++ festival/bars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Bar, BarLike
 
-# Create your views here.
+
+def bar_list(request):
+    bars = Bar.objects.all()
+    # 페이지네이션 코드
+    return render(request, 'bar_list.html', {'bars':bars})
