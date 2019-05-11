88f8410be19c6041624b4f3843e52b5177b21d1b
leemirim <leemirim9654@gmail.com>
2019-05-11 19:57:44 +0900

    Post 모델의 activate 필드 삭제

--- festival/posts/models.py
+++ festival/posts/models.py
@@ -25,7 +25,6 @@ class Post(TimeStampedModel):
     image = models.ImageField(upload_to=f'posts/', blank=True, null=True)
     text = models.TextField(max_length=200)
     how_many = models.IntegerField(default=1) # 몇대몇 미팅을 구하는지 인원수 
-    activate = models.BooleanField(default=True) # 미팅대상을 구했으면 False, 구하고 있는 중이면 True
 
     def __str__(self):
         return f'{self.title}-{self.creator}'
