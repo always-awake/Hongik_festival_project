cb5136683583fdaceda8f1ce7d6042e578d77ed2
leemirim <leemirim9654@gmail.com>
2019-05-13 21:45:21 +0900

    bars 수정

--- festival/bars/models.py
+++ festival/bars/models.py
@@ -6,10 +6,10 @@ from posts.models import TimeStampedModel
 class Bar(TimeStampedModel):
     """ Bar Model """
     name = models.CharField(max_length=80)
-    represent_image = models.ImageField(upload_to=f'bars/')
-    first_image = models.ImageField(upload_to=f'bars/')
-    second_image = models.ImageField(upload_to=f'bars/')
-    third_image = models.ImageField(upload_to=f'bars/')
+    represent_image = models.ImageField(upload_to='bars')
+    first_image = models.ImageField(upload_to='bars')
+    second_image = models.ImageField(upload_to='bars')
+    third_image = models.ImageField(upload_to='bars')
     text = models.TextField(max_length=200) # 술집에 대한 간단한 소개
     location_url = models.TextField(max_length=200) # 술집 위치 -> 이후 필요에 따라 변경될 가능성이 높은 필드
 
@@ -28,3 +28,4 @@ class BarLike(TimeStampedModel):
 
     def __str__(self):
         return f'{self.bar.name}-{self.creator.name}'
+    
