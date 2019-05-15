c3b36eed3f7fb370d4c8707ffc2c6145a041d89b
leemirim <leemirim9654@gmail.com>
2019-05-15 17:57:10 +0900

    Bar 모델 수정

--- festival/bars/models.py
+++ festival/bars/models.py
@@ -10,17 +10,25 @@ class Bar(TimeStampedModel):
     first_image = models.ImageField(upload_to='bars')
     second_image = models.ImageField(upload_to='bars')
     third_image = models.ImageField(upload_to='bars')
+    summary = models.CharField(max_length=300, null=True)
     text = models.TextField(max_length=200) # 술집에 대한 간단한 소개
     location_url = models.TextField(max_length=200) # 술집 위치 -> 이후 필요에 따라 변경될 가능성이 높은 필드
+    keyword = models.CharField(max_length=80, null=True) # 술집을 한단어로 표현하는 키워드
+    address = models.CharField(max_length=300, null=True) # 홍대 정문에서 몇 분 이런식으로
+    first_menu = models.CharField(max_length=150, null=True, blank=True) # 추천 메뉴1
+    second_menu = models.CharField(max_length=150, null=True, blank=True) # 추천 메뉴2
+    third_menu = models.CharField(max_length=150, null=True, blank=True) # 추천 메뉴3
+    fourth_menu = models.CharField(max_length=150, null=True, blank=True) # 추천 메뉴4
 
     @property
     def barlike_count(self):
         return self.likes.count()
-
+    
     def __str__(self):
         return f'{self.name}'
+    
+    
 
-# 입시 모델 - 필요에 따라 삭제될 수 있는 모델
 class BarLike(TimeStampedModel):
     """ BarLike Model """
     bar = models.ForeignKey(Bar, on_delete=models.CASCADE, related_name='likes')
